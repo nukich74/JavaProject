b5151c68e3d8616fc3a7eafe18f1fdba75864969
nukich74 <nukich74@mail.ru>
2012-12-04 18:22:45 +0400

    Wrote skeleton of logging proxy

--- Proxy/src/ru/fizteh/fivt/orlovNikita/proxy/LoggingProxyFactory.java
+++ Proxy/src/ru/fizteh/fivt/orlovNikita/proxy/LoggingProxyFactory.java
@@ -12,11 +12,41 @@ import java.lang.reflect.Proxy;
  */
 public class LoggingProxyFactory implements ru.fizteh.fivt.proxy.LoggingProxyFactory {
 
+    public static boolean isWrapperOrPrimitive(Class<?> clazz) {
+        return clazz.isPrimitive() || clazz.equals(Boolean.class) ||
+                clazz.equals(Byte.class) || clazz.equals(Character.class) ||
+                clazz.equals(Short.class) || clazz.equals(Integer.class) ||
+                clazz.equals(Long.class) || clazz.equals(Float.class) ||
+                clazz.equals(Double.class) || clazz.isEnum() || clazz.equals(String.class);
+    }
+
+
     @Override
     public Object createProxy(Object target, Appendable writer, Class... interfaces) {
         return Proxy.newProxyInstance(((Class) target).getClassLoader(),
                 new Class[]{(Class) target},
                 new InvocationHandler() {
+
+                    public StringBuilder buildArrayStringValue(StringBuilder builder, Object... array) {
+                        if (array.length == 1) {
+                            if (isWrapperOrPrimitive(array.getClass())) {
+                                builder.append(array);
+                            } else {
+                                buildArrayStringValue(builder, array[0]);
+                            }
+                        } else {
+                            builder.append(array.length).append("{");
+                            for (int i = 0; i < array.length; i++) {
+                                if (i != 0) {
+                                    builder.append(", ");
+                                }
+                                builder = buildArrayStringValue(builder, array[i]);
+                            }
+                        }
+                        return builder;
+                    }
+
+                    @Override
                     public Object invoke(Object proxy, Method method,
                                          Object[] args) throws Throwable {
                         if ((proxy == null) || (method == null) || (args == null)) {
@@ -24,11 +54,18 @@ public class LoggingProxyFactory implements ru.fizteh.fivt.proxy.LoggingProxyFac
                         } else {
                             Object ret = method.invoke(proxy, args);
                             StringBuilder builder = new StringBuilder();
+                            builder.append(method.getName()).append("(");
 
+                            for (int i = 0; i < builder.length(); i++) {
+                                if (isWrapperOrPrimitive(args[i].getClass())) {
+                                    builder.append(args[i].toString());
+                                } else if (args[i].getClass().isArray()) {
+
+                                }
+                            }
                             return ret;
                         }
                     }
                 });
     }
-}
 }
