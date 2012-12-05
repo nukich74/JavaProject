f58fc36c1e21e0bd18041619c087f44b2fb6332e
nukich74 <nukich74@mail.ru>
2012-12-05 17:57:10 +0400

    Tested, done!

--- Proxy/src/ru/fizteh/fivt/orlovNikita/proxy/LoggingProxyFactory.java
+++ Proxy/src/ru/fizteh/fivt/orlovNikita/proxy/LoggingProxyFactory.java
@@ -1,8 +1,10 @@
 package ru.fizteh.fivt.orlovNikita.proxy;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.TreeSet;
+import java.util.Collections;
+import java.util.HashSet;
 
 /**
  * Package: ru.fizteh.fivt.orlovNikita.proxy
@@ -12,15 +14,20 @@ import java.util.TreeSet;
  */
 public class LoggingProxyFactory implements ru.fizteh.fivt.proxy.LoggingProxyFactory {
 
-    private StringBuilder builder = new StringBuilder();
-    private TreeSet<Method> methods;
+    private StringBuilder builder;
+    private HashSet<Method> methods;
+
+    LoggingProxyFactory() {
+        methods = new HashSet<Method>();       ///!!!
+        builder = new StringBuilder();
+    }
 
 
     public class InvocationHandler implements java.lang.reflect.InvocationHandler {
-        private Appendable builder;
+        private Object invokeObject;
 
-        InvocationHandler(Appendable builder) {
-            this.builder = builder;
+        InvocationHandler(Object target) {
+            invokeObject = target;
         }
 
         public boolean isWrapperOrPrimitive(Class<?> clazz) {
@@ -31,70 +38,113 @@ public class LoggingProxyFactory implements ru.fizteh.fivt.proxy.LoggingProxyFac
                     clazz.equals(Double.class) || clazz.isEnum();
         }
 
-        public StringBuilder buildArrayStringValue(StringBuilder builder, Object... array) {
-            if (array.length == 1) {
-                if (isWrapperOrPrimitive(array.getClass())) {
-                    builder.append(array);
-                } else {
-                    buildArrayStringValue(builder, array[0]);
-                }
-            } else {
-                builder.append(array.length).append("{");
-                for (int i = 0; i < array.length; i++) {
+        public StringBuilder buildArrayStringValue(StringBuilder builder, Object array) {
+            if (isWrapperOrPrimitive(array.getClass())) {
+                builder.append(array);
+            } else if (array.getClass().isArray()) {
+                builder.append(((Object[]) array).length);
+                builder.append("{");
+                for (int i = 0; i < ((Object[]) array).length; i++) {
                     if (i != 0) {
                         builder.append(", ");
                     }
-                    builder = buildArrayStringValue(builder, array[i]);
+                    Object tarray = ((Object[]) array)[i];
+                    builder = buildArrayStringValue(builder, tarray);
                 }
                 builder.append("}");
+            } else {
+                builder.append(array.toString());
             }
             return builder;
+        }
+
+        void appendObject(Object o) throws Exception {
+            if (o.equals(null)) {
+                builder.append("null");
+            }
+            if (isWrapperOrPrimitive(o.getClass())) {
+                builder.append(o.toString());
+            } else if (o.getClass().isArray()) {
+                builder = buildArrayStringValue(builder, o);
+            } else if (o.getClass().equals(String.class)) {
+                String value = (String) o;
+                builder.append("\"");
+                for (int j = 0; j < value.length(); j++) {
+                        switch (value.charAt(j)) {
+                            case '\t':
+                                builder.append("\\t");
+                                break;
+                            case '\r':
+                                builder.append("\\r");
+                                break;
+                            case '\n':
+                                builder.append("\\n");
+                                break;
+                            default:
+                                builder.append(value.charAt(j));
+                                break;
+                        }
+                }
+                builder.append("\"");
+            }
+
         }
 
         @Override
         public Object invoke(Object proxy, Method method,
                              Object[] args) throws Throwable {
-            if ((proxy == null) || (method == null) || (args == null)) {
+            if ((proxy == null) || (method == null) || ((args == null) && (method.getParameterTypes().length != 0))) {
                 throw new RuntimeException("Some args are null");
             } else {
                 builder.append("\n");
-                Object ret = method.invoke(proxy, args);
-                builder.append(method.getName()).append("(");
 
-                for (int i = 0; i < args.length; i++) {
-                    if (i != 0) {
-                        builder.append(", ");
-                    }
-                    if (args[i].equals(null)) {
-                        builder.append("null");
-                    }
-                    if (isWrapperOrPrimitive(args[i].getClass())) {
-                        builder.append(args[i].toString());
-                    } else if (args[i].getClass().isArray()) {
-                        builder = buildArrayStringValue((StringBuilder) builder, args[i]);
-                    } else if (args[i].getClass().equals(String.class)) {
-                        String value = (String) args[i];
-                        builder.append("\"");
-                        for (int j = 0; j < value.length(); j++) {
-                            if (value.charAt(i) == '\\') {
-                                builder.append('\\');
+                if (methods.contains(method)) {
+                    builder.append(method.getName()).append("(");
+                    if (args == null) {
+                        builder.append(proxy.getClass().getSimpleName()).append(".").append(method.getName());
+                    } else
+                        for (int i = 0; i < args.length; i++) {
+                            if (i != 0) {
+                                builder.append(", ");
                             }
-                            builder.append(value.charAt(i));
+                            try {
+                                appendObject(args[i]);
+                            } catch (IOException e) {
+                                throw new RuntimeException("Can't append object in args!");
+                            }
                         }
-                        builder.append("\"");
+                    builder.append(")");
+                    try {
+                        Object ret = method.invoke(invokeObject, args);
+                        if (!(method.getReturnType().equals(void.class))) {
+                            builder.append("-->").append(ret.toString());
+                        }
+                        return ret;
+                    } catch (Exception e) {
+                        builder.append("threw ").append(e.getClass().toString()).append(" : ").append(e.getMessage());
+                        //throw new RuntimeException("Exception when invoke!");
                     }
+                } else {
+                    throw new RuntimeException("");
                 }
-                return ret;
             }
+            return null;
         }
     }
 
-
-
     @Override
     public Object createProxy(Object target, Appendable writer, Class... interfaces) {
-        return Proxy.newProxyInstance(((Class) target).getClassLoader(),
-                new Class[]{(Class) target}, new InvocationHandler(builder));
+        builder = (StringBuilder) writer;
+        for (Class val : interfaces) {
+            if (!val.isInterface()) {
+                throw new RuntimeException("value is not interface");
+            } else {
+                Collections.addAll(methods, val.getDeclaredMethods());
+            }
+        }
+
+        return Proxy.newProxyInstance(target.getClass().getClassLoader(),
+                interfaces, new InvocationHandler(target));
 
     }
 
--- Proxy/src/ru/fizteh/fivt/orlovNikita/proxy/LoggingProxyFactoryTest.java
+++ Proxy/src/ru/fizteh/fivt/orlovNikita/proxy/LoggingProxyFactoryTest.java
@@ -2,7 +2,8 @@ package ru.fizteh.fivt.orlovNikita.proxy;
 
 import org.junit.Test;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Package: ru.fizteh.fivt.orlovNikita.proxy
@@ -13,9 +14,31 @@ import java.util.HashMap;
 public class LoggingProxyFactoryTest {
     @Test
     public void test1() {
+        ArrayList<Integer[][]> map = new ArrayList<Integer[][]>();
         StringBuilder builder = new StringBuilder();
-        Object invoker = new LoggingProxyFactory().createProxy(new HashMap <Integer[], String>().getClass(), builder);
-
+        LoggingProxyFactory invoker = new LoggingProxyFactory();
+        List proxy = (List) invoker.createProxy(map, builder, List.class);
+        proxy.add(new Integer[][]{{1, 2}, {4, 5}});
+        proxy.add(new Integer[][]{{1, 2}, {8, 9}});
+        proxy.add(new Integer[][]{{3, 2}, {5, 6}});
+        proxy.indexOf(new Integer[][]{{1, 2}, {0, 9}});
+        proxy.get(0);
+        proxy.get(123);
+        System.out.println(invoker.getLog());
     }
+
+    @Test
+    public void test2() {
+        ArrayList<String> map = new ArrayList<String>();
+        StringBuilder builder = new StringBuilder();
+        LoggingProxyFactory factory = new LoggingProxyFactory();
+        List proxy = (List) factory.createProxy(map, builder, List.class);
+        proxy.add("asd\n");
+        proxy.add("\fasdasd\t\n\\");
+        proxy.indexOf("asd\n");
+        proxy.clear();
+        System.out.println(factory.getLog());
+    }
+
 
 }
